276590603c068d37d4a87e6d08b1a703cf3bdb30
Osman <osmyasal@gmail.com>
2024-02-11 02:51:00 +0300

    Add files via upload
    ..

---
+++ optkit_cwrapper.cc
@@ -0,0 +1,14 @@
+#include <optkit.hh>
+
+extern "C"
+{
+	int main_c(int argc, char** argv);
+}
+
+int main(int argc, char** argv)
+{
+	OptimizerKit optkit{false};
+	OPTKIT_RAPL(main_block,argv[0]);
+
+	return main_c(argc,argv);
+}
