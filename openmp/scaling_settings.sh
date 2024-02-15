15a5bfe85bb54c0b52804e60bd89bfe1c238c5a9
rt7 <oyasal@gmail.com>
2024-02-15 03:45:45 +0300

    ..

--- openmp/scaling_settings.sh
+++ openmp/scaling_settings.sh
@@ -5,16 +5,18 @@ hostname=$(hostname)
 # Check if the hostname matches a specific pattern
 
 cores="0-$(nproc)"	## get maximum by default
+max_cores = $(nproc)
 
 if [[ $hostname == "user-HP-Z840-Workstation" ]]; then
     cores="10-19,30-39"	## get second socket
-    echo "SYSTEM_NAME: user-HP-Z840-Workstation cores: $cores"
+    max_cores=$(($max_cores / 2))
+    echo "SYSTEM_NAME: user-HP-Z840-Workstation cores: $cores -- $max_cores"
     
 elif [[ $hostname == "rt7-laptop-vivo" ]]; then
     cores="0-8"
-    echo "SYSTEM_NAME: rt7-laptop-vivo cores: $cores"
+    echo "SYSTEM_NAME: rt7-laptop-vivo cores: $cores -- $max_cores"
     
 else
-    echo "SYSTEM_NAME: UNKNOWN cores: $cores"
+    echo "SYSTEM_NAME: UNKNOWN cores: $cores -- $max_cores"
 fi
 
